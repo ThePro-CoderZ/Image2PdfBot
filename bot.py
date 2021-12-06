34047c20b7bab50538a098864c64c0996ff67aaa
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-06 09:51:57 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,71 @@
+import os
+from PIL import Image
+from pyrogram import Client,filters 
+from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
+
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
+TOKEN = os.environ.get("TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", 12345))
+
+API_HASH = os.environ.get("API_HASH", "")
+app = Client(
+        "pdf",
+        bot_token=TOKEN,api_hash=API_HASH,
+            api_id=API_ID
+    )
+
+
+LIST = {}
+
+@app.on_message(filters.command(['start']))
+async def start(client, message):
+ await message.reply_text(text =f"""Hello {message.from_user.first_name}, I'm image to pdf bot..
+i can convert image to pdf""",reply_to_message_id = message.message_id ,  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/TheArjvps") ],
+                 [InlineKeyboardButton("Repo 🇮🇳", url="https://gitHub.com/Akshat7678/image2pdfbot") ]       ]        ) )
+
+
+
+
+@app.on_message(filters.private & filters.photo)
+async def pdf(client,message):
+ 
+ if not isinstance(LIST.get(message.from_user.id), list):
+   LIST[message.from_user.id] = []
+
+  
+ 
+ file_id = str(message.photo.file_id)
+ ms = await message.reply_text("Converting to PDF ......")
+ file = await client.download_media(file_id)
+ 
+ image = Image.open(file)
+ img = image.convert('RGB')
+ LIST[message.from_user.id].append(img)
+ await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
+ 
+
+@app.on_message(filters.command(['convert']))
+async def done(client,message):
+ images = LIST.get(message.from_user.id)
+
+ if isinstance(images, list):
+  del LIST[message.from_user.id]
+ if not images:
+  await message.reply_text( "No image !!")
+  return
+
+ path = f"{message.from_user.id}" + ".pdf"
+ images[0].save(path, save_all = True, append_images = images[1:])
+ 
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here your pdf !!")
+ os.remove(path)
+ 
+ 
+ 
+ 
+app.run()
